91e62fc7e9a529cacf8d958c205f121afd039b03
Troy Pracy <troyp7@gmail.com>
2021-11-28 23:53:13 +0000

    Add personal_skeleton.js, rename user.mylatlong to user.latlong

---
+++ personal/personal_skeleton.js
@@ -0,0 +1,26 @@
+var user = {
+    name:       "",
+    firstname:  "",
+    lastname:   "",
+    address:    "",
+    street:     "",
+    suburb:     "",
+    state:      "",
+    postcode:   "",
+    latlong:    "",
+    ebay:
+    {
+        savedSellers_trksid: "",
+    },
+    phone:      "",
+    mobile:     "",
+    masterpw:   "",
+    email:      "",
+    email2:     "",
+    bankurl:    "",
+    creditcard: "",
+    openexchangeratesappid: "",
+    currencyconverterapikey: "",
+}
+
+window.user = user;
