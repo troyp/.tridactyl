e6f1a957bd7b638a9f6b816854a367f6d78709b4
Troy Pracy <troyp7@gmail.com>
2021-12-03 09:24:49 +0000

    Move convertUnits(), sunriseSunset() to new file apps.js
    Also, fix errors in :convertunits (missing space, wrong function name)

--- utils.js
+++ utils.js
@@ -1,30 +1,5 @@
 var utils = {
 
-    convertUnits: function(args) {
-        args = args.filter(a=> a.trim()!== "");
-        var n, unit1, unit2;
-        if (args.length==1) {
-            return tri.native.run(`units -t ${args[0]}`).then(
-                res => res.content
-            );
-        } else {
-            if (args.length==2) {
-                var match = String(args[0]).match(/([0-9.*/+-]*)(.*)/);
-                n = match[1]||"1";
-                unit1 = match[2];
-                unit2 = args[1];
-            } else {
-                [n, unit1, unit2] = args;
-            }
-            return tri.native.run(`units -t "${eval(n)} ${unit1}" ${unit2}`).then(
-                res => {
-                    if (res.code==0) return n + unit1 + " = " + res.content + unit2;
-                    else return `ERROR code ${res.code}:${res.content}\n\tcommand: ${res.command}`;
-                }
-            );
-        }
-    },
-
     message: function(s, opts={}) {
         var s_ = (opts.prefix || "") + s;
         if (opts.temp) {
@@ -34,11 +9,6 @@ var utils = {
             tri.excmds.fillcmdline_nofocus(s_);
         }
         return s;
-    },
-
-    sunriseSunset: function(lat, long) {
-        var times = SunCalc.getTimes(new Date(), lat, long);
-        return [sunrise, sunset] = [times.sunrise, times.sunset].map(t=>t.toTimeString());
     },
 
     tab: {
