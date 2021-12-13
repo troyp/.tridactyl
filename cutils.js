c64821fd4b3661506345485fb39914165a48792d
Troy Pracy <troyp7@gmail.com>
2021-12-13 09:22:37 +0000

    Add commands :withcutils and :urltoggle

--- cutils.js
+++ cutils.js
@@ -81,6 +81,31 @@ var cutils = {
         return s;
     },
 
+    /* urltoggle(s1, s2, url)                Replace s1 with s2, or else s2 with s1
+     * urltoggle(s1, s2, url, {re1, re2})    Replace regex1 with s2, or else regex2 with s1
+     */
+    urltoggle: function(s1, s2, url, opts={}) {
+        var patt1 = opts.re1 || s1;
+        var patt2 = opts.re2 || s2;
+        if (url.includes(patt1))
+            return url.replace(patt1, s2);
+        else
+            return url.replace(patt2, s1);
+    },
+
+    urltoggleWrapper: function(argstr) {
+        var args = argstr.trim().split(/ +/);
+        [s1, s2, ...opts] = args;
+        var restr1, flags1, restr2, flags2;
+        if (opts.length==4) [restr1, flags1, restr2, flags2] = opts;
+        else [restr1, restr2, flags1, flags2] = opts;
+        var re1 = restr1 ? RegExp(restr1, flags1||"") : null;
+        var re2 = restr2 ? RegExp(restr2, flags2||"") : null;
+        var url = window.location.href;
+        var newUrl = this.urltoggle(s1, s2, url, {re1: re1, re2: re2});
+        if (newUrl && newUrl!==url) window.location.replace(newUrl);
+    },
+
     yankWithMsg: function(s, opts={}) {
         tri.excmds.yank(s);
         var defaultPrefix = "Copied" + (opts.useAlert ? "...\n" : ": ");
