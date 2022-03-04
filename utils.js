troyp/.tridactyl

commit 963b0079dd0e01bf9ff8db8a0f1711c539eba0a2

utils.tri.searchurlResolve(): fix result for empty search term

utils.js
@@ -884,14 +884,10 @@ utils.tri = {
     searchurlResolve: function(url, opts={}) {
         opts = this.parseOpts(opts, {castBoolean: "default"});
         url = url.trim();
-        const k = url.indexOf(" ");
         const defaultSE = tri.config.get("searchengine") || "google";
-        const SE = opts.default
-              ? defaultSE
-              : url.slice(0, k);
-        const search = opts.default
-              ? url
-              : url.slice(k).trim();
+        const [SE, search] = opts.default
+              ? [defaultSE, url]
+              : url.split(/ +/).map(trim);
         const su = tri.config.get("searchurls")[SE];
         return su.includes("%s")
             ? su.replace(/%s/g, search)

commit cecd5370d6930d10550566a54513ebd048fe2e96

Fix searchurlResolve(), broken in commit 963b007
Commit 963b007 fixed empty searches with engines, but broke multi-word searches
(since url.split() splits on every occurrence of a regex, not just the first.
Additionally, the trim callback `trim` was incorrect ( should have been
`s=>String.prototype.trim.bind(s)`) and wasn't required anyway.

utils.js
@@ -902,7 +902,7 @@ utils.tri = {
         const defaultSE = tri.config.get("searchengine") || "google";
         const [SE, search] = opts.default
               ? [defaultSE, url]
-              : url.split(/ +/).map(trim);
+              : S.splitFirst(url, / +/);
         const su = tri.config.get("searchurls")[SE];
         return su.includes("%s")
             ? su.replace(/%s/g, search)