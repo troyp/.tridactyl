e64cf59f30b545c1797ce275bc5d2a76b024b39f
Troy Pracy <troyp7@gmail.com>
2022-03-14 18:52:16 +1100

    Add alias S.matchSC() for S.matchSmartCase()

--- S.js
+++ S.js
@@ -42,6 +42,7 @@ S = {
         let s2adj = this.isLowerCase(s) ? s2.toLowerCase() : s2;
         return s2adj.match(s);
     },
+    matchSC: (s, s2) => S.matchSmartCase(s, s2),
     capitalize: function(s) {
         return s.replace(/\b\w/g, function (c) { return c.toUpperCase(); });
     },
