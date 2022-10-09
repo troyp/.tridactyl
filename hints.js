11823ec28e43af6d644d157d3406c14850382c61
Troy Pracy <troyp7@gmail.com>
2022-10-10 08:08:20 +1100

    Add hints.js file (omitted from commit 0589107)

---
+++ hints.js
@@ -0,0 +1,60 @@
+// ───────────────────────────────────────────────────────────────────────────────
+// ╭────────────╮
+// │            │
+// │  HINTS.js  │
+// │            │
+// ╰────────────╯
+
+const hints = {
+
+    follow: async function(opts={}) {
+        opts = utils.tri.parseOpts(opts, {castArray: "extraSelectors"});
+        const switches = opts.switches || "";
+        const selectors = tri.config.get("hinttags.default").concat(opts.extraSelectors);
+        hint(switches, "-c", selectors.join(","));
+    },
+
+};
+
+tri.config.set(
+    "hinttags.default", [
+        "input:not([type=hidden]):not([disabled])",
+        "a",
+        "area",
+        "button",
+        "details",
+        "iframe",
+        "label",
+        "select",
+        "summary",
+        "textarea",
+        "label",
+        "th",
+        "[onclick]",
+        "[onmouseover]",
+        "[onmousedown]",
+        "[onmouseup]",
+        "[oncommand]",
+        "[role='link']",
+        "[role='button']",
+        "[role='checkbox']",
+        "[role='combobox']",
+        "[role='listbox']",
+        "[role='listitem']",
+        "[role='menuitem']",
+        "[role='menuitemcheckbox']",
+        "[role='menuitemradio']",
+        "[role='option']",
+        "[role='radio']",
+        "[role='scrollbar']",
+        "[role='slider']",
+        "[role='spinbutton']",
+        "[role='tab']",
+        "[role='textbox']",
+        "[role='treeitem']",
+        "[class*='button']",
+        "[tabindex]",
+    ],
+);
+
+window.hints = hints;
