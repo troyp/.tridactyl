4823f642465bb3470e3317538f4732db24359790
Troy Pracy <troyp7@gmail.com>
2022-10-11 05:19:16 +1100

    cleanup.c.js: order cases, add netflix.com

--- sites/cleanup.c.js
+++ sites/cleanup.c.js
@@ -5,17 +5,50 @@
 function cleanup() {
     const url = document.location.href;
     if (false) {
-    } else if (url.match(/https:\/\/github\.com\/.*\/issues\//)) {
-        keep("div#show_issue");
-        rmall("img.avatar", "div.sidebar-notifications", ".gh-header-actions",
-              "#issue-comment-box", ".discussion-timeline-actions");
-    } else if (url.match(/https:\/\/janeblundellart\.blogspot\.com/)) {
+    } else if (url.match(/https:\/\/www.codeproject\.com/)) {
+        /* codeproject.com */
+        keep(".header", "#contentdiv");
+        rmall("img.avatar");
+    } else if (url.match(/https:\/\/onepiece-online-manga\.com/)) {
+        /* onepiece-online-manga.com */
+        keep("article");
+        rmall(".simplesocialbuttons");
+    } else if (url.match(/https:\/\/bartoszmilewski.com/))  {
+        /* bartoszmilewski.com */
         keep("#main");
-        rmall("iframe", "script", "#blog-pager");
-    } else if (url.match(/https:\/\/poets\.org\/poem\//)) {
-        keep(".card-header", ".card-body");
-        rmall(".poem__actions");
+        rmall(".comment-author.vcard>img");
+    } else if (url.match(/https:\/\/patternsinfp.wordpress.com/))  {
+        /* patternsinfp.wordpress.com */
+        keep("#site-title,#content");
+        rmall("#nav-above,#nav-below,#jp-post-flair,.reply,.post.pingback,#respond,#eu-cookie-law");
+    } else if (url.match(/https:\/\/collider\.com/)) {
+        /* collider.com */
+        /* fix image height */
+        keep("article:first-of-type");
+        rmall("aside", ".heading_breadcrumb", ".heading_sharing", "span.next-single");
+        $$("figure").forEach(f=>f.style.height=f.querySelector("img").height+"px");
+    } else if (url.match(/https:\/\/www\.dndbeyond\.com/)) {
+        /* dndbeyond.com */
+        // keep(".p-article-content");
+        keep("header.page-header", "div.container", "ul.quick-menu");
+        rmall("aside.tasha-notes");
+        $$("aside").forEach(e => {
+            e.style.border = "1px solid black";
+            e.style.borderRadius = "15px";
+            e.style.margin = "2em 10em";
+            e.style.padding = "1em";
+            e.style.backgroundColor = "rgba(252, 249, 246, 1)";
+        });
+    } else if (url.match(/https?:\/\/dnd5e\.wikidot\.com/)) {
+        /* dnd5e.wikidot.com */
+        keep(".main-content-wrap");
+        rm([".site-bar", ".mm-mega-menu"], "firstMatch");
+    } else if (url.match(/https:\/\/dzone.com/))  {
+        /* dzone */
+        rmall("iframe","script",".header",".announcementBar",".breadcrumb-padding",".signin-prompt",
+              ".related","#adtoniq-msgr-bar");
     } else if (url.match(/https:\/\/[^/]+\.fandom\.com\//) || url.match(/https:\/\/[^/]+\.wikia\.com\//)) {
+        /* fandom.com */
         /* FIXME */
         rmall(
             '#WikiaTopAds', '.wikia-ad', '#WikiaArticleBottomAd',
@@ -30,7 +63,15 @@ function cleanup() {
         const divs = Array.from(content.document.getElementsByTagName('div'));
         const videodivs = divs.filter(e=>e.getAttribute("itemtype")=="http://schema.org/VideoObject");
         videodivs.forEach(e=>e.parentNode.removeChild(e));
+    } else if (url.match(/https:\/\/www\.geeksforgeeks\.org/)) {
+        /* geeksforgeeks */
+        if (cleanupDisqus()) return;
+        keep(".leftSideBarParent", "#primary");
+        rmall("iframe[allow*=autoplay]", ".entry-meta", ".upvoteArticle", ".nav-single");
+        removeSelectorFiltered("div", e=>e.id.match(/^AP_G4GR/));
+        getFirstSelector("#primary").style.width="70%";
     } else if (url.match(/https:\/\/forums\.giantitp\.com/)) {
+        /* gianttip.com */
         keep("#content");
         rmall("h2>img", "#above_postlist", "#thread_controls", "#below_postlist", ".navlinks", "#thread_info", ".userinfo img");
         $1("html").style.background = "white";
@@ -44,76 +85,59 @@ function cleanup() {
         hd1.style.padding = "5px";
         hd1.style.border = "black solid 1px";
         hd1.style.borderRadius = "1px";
+    } else if (url.match(/https:\/\/github\.com\/.*\/issues\//)) {
+        /* github */
+        keep("div#show_issue");
+        rmall("img.avatar", "div.sidebar-notifications", ".gh-header-actions",
+              "#issue-comment-box", ".discussion-timeline-actions");
+    } else if (url.match(/https:\/\/www\.grammarly\.com/)) {
+        /* grammarly */
+        rmall("._1X8pZ-overlay", "header", "._3fq8S-container", "._2Szu7-avatar", "._3uFk6-banner",
+              ".tool__product", "._16iRt-container", "section.F0u0G-container", "._1hrQx-container", "footer");
     } else if (url.match(/https:\/\/wiki.haskell\.org/)) {
+        /* haskell wiki */
         keep("#content");
         rmall("#catlinks");
-    } else if (url.match(/https:\/\/www.codeproject\.com/)) {
-        keep(".header", "#contentdiv");
-        rmall("img.avatar");
-    } else if (url.match(/https:\/\/onepiece-online-manga\.com/)) {
-        keep("article");
-        rmall(".simplesocialbuttons");
-    } else if (url.match(/https:\/\/bartoszmilewski.com/))  {
+    } else if (url.match(/https:\/\/janeblundellart\.blogspot\.com/)) {
+        /* janeblundellart */
         keep("#main");
-        rmall(".comment-author.vcard>img");
-    } else if (url.match(/https:\/\/patternsinfp.wordpress.com/))  {
-        keep("#site-title,#content");
-        rmall("#nav-above,#nav-below,#jp-post-flair,.reply,.post.pingback,#respond,#eu-cookie-law");
-    } else if (url.match(/https:\/\/dzone.com/))  {
-        rmall("iframe","script",".header",".announcementBar",".breadcrumb-padding",".signin-prompt",
-              ".related","#adtoniq-msgr-bar");
+        rmall("iframe", "script", "#blog-pager");
     } else if (url.match(/https:\/\/www\.japanesewithanime\.com/)) {
+        /* japanesewithanime */
         keep("article");
         $1("article").style.padding = "1em";
         rmall(".social-buttons-strip", "aside#sub-fold-ad", ".categoryNavbox", ".comment-footer",
               ".avatar-image-container", ".comment-reply", "#top-continue", ".article-end-footer");
+    } else if (url.match("https://www.mapsofworld.com")) {
+        /* mapsofworld.com */
+        keep("main");
+        $1("main").style.paddingTop = "0";
+        $1("section").style.marginTop = "0";
+    } else if (url.match(/https:\/\/martinfowler\.com/)) {
+        /* martinfowler.com */
+        rmall("#banner", "#page-footer", ".author-photo");
     } else if (url.match(/https:\/\/opensource\.com/)) {
+        /* opensource.com */
         keep("#article-template");
         rmall(".os-article__image", ".os-article__left", ".os-article__bottom",
               ".authorbio__thumbnail", ".user-picture-badges");
-    } else if (url.match(/https:\/\/martinfowler\.com/)) {
-        rmall("#banner", "#page-footer", ".author-photo");
+    } else if (url.match(/https:\/\/www\.netflix\.com\//)) {
+        rmall(".static-image.image-layer", ".billboard-row", ".spotlight");
+    } else if (url.match(/https:\/\/poets\.org\/poem\//)) {
+        /* poets.org */
+        keep(".card-header", ".card-body");
+        rmall(".poem__actions");
+    } else if (url.match("https://rpgbot.net")) {
+        /* rpgbot.net */
+        keep("article");
+        $1("div>span[itemprop='name']").parentElement.remove();
+        rmall(".topad", ".shareit");
     } else if (url.match(/https:\/\/blog\.sigplan\.org/)) {
+        /* sigplan.org */
         if (cleanupDisqus()) return;
         keep("article");
         rmall(".et_post_meta_wrapper>img");
         $1("body").style.margin = "3em";
-    } else if (url.match(/https:\/\/www\.geeksforgeeks\.org/)) {
-        if (cleanupDisqus()) return;
-        keep(".leftSideBarParent", "#primary");
-        rmall("iframe[allow*=autoplay]", ".entry-meta", ".upvoteArticle", ".nav-single");
-        removeSelectorFiltered("div", e=>e.id.match(/^AP_G4GR/));
-        getFirstSelector("#primary").style.width="70%";
-    } else if (url.match(/https:\/\/www\.grammarly\.com/)) {
-        rmall("._1X8pZ-overlay", "header", "._3fq8S-container", "._2Szu7-avatar", "._3uFk6-banner",
-              ".tool__product", "._16iRt-container", "section.F0u0G-container", "._1hrQx-container", "footer");
-    } else if (url.match(/https:\/\/collider\.com/)) {
-        /* fix image height */
-        keep("article:first-of-type");
-        rmall("aside", ".heading_breadcrumb", ".heading_sharing", "span.next-single");
-        $$("figure").forEach(f=>f.style.height=f.querySelector("img").height+"px");
-    } else if (url.match(/https?:\/\/dnd5e\.wikidot\.com/)) {
-        keep(".main-content-wrap");
-        rm([".site-bar", ".mm-mega-menu"], "firstMatch");
-    } else if (url.match(/https:\/\/www\.dndbeyond\.com/)) {
-        // keep(".p-article-content");
-        keep("header.page-header", "div.container", "ul.quick-menu");
-        rmall("aside.tasha-notes");
-        $$("aside").forEach(e => {
-            e.style.border = "1px solid black";
-            e.style.borderRadius = "15px";
-            e.style.margin = "2em 10em";
-            e.style.padding = "1em";
-            e.style.backgroundColor = "rgba(252, 249, 246, 1)";
-        });
-    } else if (url.match("https://rpgbot.net")) {
-        keep("article");
-        $1("div>span[itemprop='name']").parentElement.remove();
-        rmall(".topad", ".shareit");
-    } else if (url.match("https://www.mapsofworld.com")) {
-        keep("main");
-        $1("main").style.paddingTop = "0";
-        $1("section").style.marginTop = "0";
     } else {
         cleanupGeneric();
     }
