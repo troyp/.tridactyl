c72346c9f687dff5ba374e7885ef15b21e09ba96
Troy Pracy <troyp7@gmail.com>
2022-10-10 21:31:13 +1100

    disneyp: update <Space> key binding

---
+++ sites/video.c.js
@@ -0,0 +1,18 @@
+// ───────────────────────────────────────────────────────────────────────────────
+// ╭───────────────╮
+// │ MODULE: video │
+// ╰───────────────╯
+
+vid = {};
+
+vid.disneyp = {
+    go: function() {
+        const url = document.location.href;
+        if (url.match(/\/video\//))
+            cutils.click1("button.play-pause-icon") || cutils.get1("button", /play next episode/i);
+        else if (url.match(/\/series\//))
+            cutils.click1("button", /continue/i);
+    },
+
+};
+
