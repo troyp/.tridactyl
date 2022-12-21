987b1abb5d1ea6a8f75c88995cbb78062f34d93f
Troy Pracy <troyp7@gmail.com>
2022-12-22 09:06:54 +1100

    Disney Prime: binding cc (play video/play next)

--- sites/video.c.js
+++ sites/video.c.js
@@ -9,6 +9,16 @@ vid.disneyp = {
     go: function() {
         const url = document.location.href;
         if (url.match(/\/video\//))
+            return cutils.click1('button[data-testid="up-next-play-button"]')
+            || cutils.click1("button.play-pause-icon")
+            || cutils.get1("button", /play next episode/i);
+        else if (url.match(/\/series\//))
+            return cutils.click1("button", /continue/i);
+    },
+
+    playpause: function() {
+        const url = document.location.href;
+        if (url.match(/\/video\//))
             cutils.click1("button.play-pause-icon") || cutils.get1("button", /play next episode/i);
         else if (url.match(/\/series\//))
             cutils.click1("button", /continue/i);
