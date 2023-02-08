troyp/.tridactyl

commit f2333cc477a695133bd3b0f3a54aa29c9536a133

places.hist.get(): allow search text or days ago as single argument
places.bm.get(): add missing declaration.

places.js
@@ -32,7 +32,7 @@ places.bm = {
     },
 
     get: async function(opts={}) {
-        args = utils.tri.parseArgs(opts.args, {type: "string"});
+        const args = utils.tri.parseArgs(opts.args, {type: "string"});
         opts = utils.tri.parseOpts(
             opts,
             { castString: "switches",
@@ -222,6 +222,8 @@ places.hist = {
             nullishDefaults: {
                 prompt: "Select tabs (S-Enter): ",
             },
+            castString: "text",
+            castNumber: daysAgo,
         });
         /* time range of results */
         if (!opts.hoursAgo && !opts.startTime && !opts.endTime)

commit 482ae1544b40ee477459352daafb35bba8f1b846

places.hist.get(): add missing quotes (fixes error introduced in f2333cc)

places.js
@@ -223,7 +223,7 @@ places.hist = {
                 prompt: "Select tabs (S-Enter): ",
             },
             castString: "text",
-            castNumber: daysAgo,
+            castNumber: "daysAgo",
         });
         /* time range of results */
         if (!opts.hoursAgo && !opts.startTime && !opts.endTime)