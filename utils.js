troyp/.tridactyl

commit 40afbdce0e04797b1dc6a11e438eac9eba789568

utils.tab: add openorswitchWr(), openorsummonWr()

utils.js
@@ -400,6 +400,11 @@ utils.tab = {
         }
     },
 
+    openorsummonWr: async function(argscount, where, whereAlt, opts={}) {
+        const [args, count] = utils.tri.parseArgsAndCount(argscount);
+        return this.openOrSwitch(args[0], {where: (count ? whereAlt : where), ...opts});
+    },
+
     /**   openOrSwitch(URL, { OPTIONS... })
      *    openOrSwitch(URL, WHERE)
      *  opts.where ["last" (default), "here", "related", "next"]: where to open new tab (if any)
@@ -427,6 +432,11 @@ utils.tab = {
                 });
             } else return this.open(url, {where: opts.where||"related"});
         }
+    },
+
+    openorswitchWr: async function(argscount, where, whereAlt, opts={}) {
+        const [args, count] = utils.tri.parseArgsAndCount(argscount);
+        return this.openOrSwitch(args[0], {where: (count ? whereAlt : where), ...opts});
     },
 
     parseTabnum: async function(n) {

commit 49d8edf3348cd2cbb6d561b0d6afccd8b9fea0f6

utils.tab.openorsummonWr(): fix call openOrSwitch() -> openOrSummon()
Fixes error in :openorsummonc introduced in revision 40afbdc

utils.js
@@ -437,7 +437,7 @@ utils.tab = {
 
     openorsummonWr: async function(argscount, where, whereAlt, opts={}) {
         const [args, count] = utils.tri.parseArgsAndCount(argscount);
-        return this.openOrSwitch(args[0], {where: (count ? whereAlt : where), ...opts});
+        return this.openOrSummon(args[0], {where: (count ? whereAlt : where), ...opts});
     },
 
     /**   openOrSwitch(URL, { OPTIONS... })