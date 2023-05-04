33ddefe1ff13f85446bdcbd3e3cb8e2a8dc7e2ac
Troy Pracy <troyp7@gmail.com>
2023-05-05 00:23:10 +1000

    dndbeyond: add binding crso (:dndbspell_lvl_cls)

--- sites/dnd.js
+++ sites/dnd.js
@@ -33,6 +33,32 @@ const dndb = {
         return [section, items];
     },
 
+    getClsNum: function(cls="") {
+        const table = {
+            "": "",
+            1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 252717:252717,
+            "artificer": 252717,
+            "bard": 1,
+            "cleric": 2,
+            "druid": 3,
+            "paladin": 4,
+            "ranger": 5,
+            "sorcerer": 6,
+            "warlock": 7,
+            "wizard": 8,
+        };
+        return table[cls];
+    },
+
+    gotoSpellLvlCls: async function(lvl, cls="") {
+        lvl ??= "";
+        const clsNum = this.getClsNum(cls);
+        const clsFilter = `filter-class=${clsNum}`;
+        const lvlFilter = `filter-level=${lvl}`;
+        const url = `https://www.dndbeyond.com/spells?${clsFilter}&${lvlFilter}`;
+        return tri.controller.acceptExCmd(`toposu! ${url}`);
+    },
+
     spellUrl: async function(name, opts={}) {
         name = name.trim();
         const firstLetter = name[0];
