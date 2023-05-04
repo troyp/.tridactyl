b36417971f3eb096e78a098d88c7c0cd6a3dda3a
Troy Pracy <troyp7@gmail.com>
2023-05-04 23:56:58 +1000

    Formatting

--- sites/dnd.js
+++ sites/dnd.js
@@ -1,47 +1,51 @@
+// ───────────────────────────────────────────────────────────────────────────────
+// ╭────────────╮
+// │ MODULE dnd │
+// ╰────────────╯
+
 const dndb = {
 
-        _site: "dndbeyond.com",
+    _site: "dndbeyond.com",
 
-        follow: async function(switches="-J") {
-            return hints.follow({
-                switches: switches,
-                extraSelectors: [
-                    ".collapsible-header",
-                    ".ddbc-tab-list__nav-item",
-                    ".ddbc-collapsible",
-                    "#open-indicator",
-                    ".list-row-indicator",
-                    ".class-spell-list-header",
-                    ".ddb-market-checkout-form-address-same-input",
-                    ".ddb-search-filter-item-wrapper",
-                    ".dice_notification_control",
-                    ".dice-toolbar--hover dice-toolbar__dropdown-die",
-                    "span.ct-sidebar__control",
-                ]
-            });
-        },
+    follow: async function(switches="-J") {
+        return hints.follow({
+            switches: switches,
+            extraSelectors: [
+                ".collapsible-header",
+                ".ddbc-tab-list__nav-item",
+                ".ddbc-collapsible",
+                "#open-indicator",
+                ".list-row-indicator",
+                ".class-spell-list-header",
+                ".ddb-market-checkout-form-address-same-input",
+                ".ddb-search-filter-item-wrapper",
+                ".dice_notification_control",
+                ".dice-toolbar--hover dice-toolbar__dropdown-die",
+                "span.ct-sidebar__control",
+            ]
+        });
+    },
 
-        getContentGroup: async function(elt) {
-            const section = elt.firstChild.firstChild.firstChild.getAttribute("data-testid");
-            const itemElts = [...elt.children[1].getElementsByClassName("ct-inventory-item")];
-            const items = itemElts.map(e=>e.getAttribute("data-testid"));
-            return [section, items];
-        },
+    getContentGroup: async function(elt) {
+        const section = elt.firstChild.firstChild.firstChild.getAttribute("data-testid");
+        const itemElts = [...elt.children[1].getElementsByClassName("ct-inventory-item")];
+        const items = itemElts.map(e=>e.getAttribute("data-testid"));
+        return [section, items];
+    },
 
-        spellUrl: async function(name, opts={}) {
-            name = name.trim();
-            const firstLetter = name[0];
-            const id = S.toTitleCase(name).replace(/ +/g, "");
-            return `https://www.dndbeyond.com/sources/phb/spell-descriptions-c#${id}`;
-        },
+    spellUrl: async function(name, opts={}) {
+        name = name.trim();
+        const firstLetter = name[0];
+        const id = S.toTitleCase(name).replace(/ +/g, "");
+        return `https://www.dndbeyond.com/sources/phb/spell-descriptions-c#${id}`;
+    },
 
-        yankContentGroup: async function(elt) {
-            const [section, items] = await this.getContentGroup();
-            cutils.yank(`${section}:\n${items.join("\n")}`);
-        },
+    yankContentGroup: async function(elt) {
+        const [section, items] = await this.getContentGroup();
+        cutils.yank(`${section}:\n${items.join("\n")}`);
+    },
 
 };
-
 sites.dndb = dndb;
 
 sites.LISTS.dnd_sites = [
