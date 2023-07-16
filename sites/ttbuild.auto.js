4a382c33c61d66c7f562ba1ebc9308da4727ab7e
Karey Higuera <karey.higuera@gmail.com>
2023-07-16 22:31:51 +1000

    Add HTML-table->markdown code from table-to-markdown extension
    Slightly modified for use in tridactyl.
    
    Co-authored-by: Troy Pracy <troyp7@gmail.com>

--- sites/ttbuild.auto.js
+++ sites/ttbuild.auto.js
@@ -1,4 +1,6 @@
 
+const url = document.location.href;
+
 // Remove clutter
 rmall(".widget_text.widget_custom_html");
 
@@ -25,14 +27,70 @@ h3s.forEach(e => {
     wrapin(e, link);
 });
 
+const stars = [
+    ["", ""],
+    ["★☆☆☆☆", "#cc0000"],
+    ["★★☆☆☆", "#ff7700"],
+    ["★★★☆☆", "#e6ad00"],
+    ["★★★★☆", "green"],
+    ["★★★★★", "blue"],
+];
+
+function ratingIndex(rating) {
+    const match = rating.match(/[★☆]+/)?.[0];
+    switch (match) {
+        case "★☆☆☆☆": return 1; break;
+        case "★★☆☆☆": return 2; break;
+        case "★★★☆☆": return 3; break;
+        case "★★★★☆": return 4; break;
+        case "★★★★★": return 5; break;
+        default: return 0;
+    }
+}
+function ratingColor(rating) {
+    const n = ratingIndex(rating);
+    return stars[n][1];
+}
+
 // Add rating colors in sidebar TOC lists
 $$(".toc_widget_list li>a").forEach(a=>{
-    const match = a.textContent.trim().match(/\(([★☆]+)\)/);
-    switch (match?.[1]) {
-        case "★☆☆☆☆": a.style.color = "#cc0000"; break;
-        case "★★☆☆☆": a.style.color = "#ff7700"; break;
-        case "★★★☆☆": a.style.color = "#e6ad00"; break;
-        case "★★★★☆": a.style.color = "green"; break;
-        case "★★★★★": a.style.color = "blue"; break;
-    }
+    const color = ratingColor(a.textContent);
+    a.style.color = color;
 });
+
+if (url.match(/https:\/\/tabletopbuilds.com\/optimized-feat-guide-for-dd-5e/)) {
+    let table = document.getElementsByTagName("table")[0];
+    let rows = [...table.rows].slice(2);
+    const summary = document.createElement("table");
+    summary.id = "summary-table";
+    table.parentElement.appendChild(summary);
+    const thead = document.createElement("thead");
+    summary.appendChild(thead);
+    let tr = document.createElement("tr");
+    const RatingCell = document.createElement("th");
+    RatingCell.textContent = "Rating";
+    const FeatCell = document.createElement("th");
+    FeatCell.textContent = "Feats";
+    tr.appendChild(RatingCell);
+    tr.appendChild(FeatCell);
+    thead.appendChild(tr);
+    const tbody = document.createElement("tbody");
+    summary.appendChild(tbody);
+    let i;
+    for (i of [5,4,3,2,1]) {
+        let tr = document.createElement("tr");
+        tr.style.color = stars[i][1];
+        tbody.appendChild(tr);
+        let th = document.createElement("th");
+        th.textContent = stars[i][0];
+        tbody.rows[5-i].appendChild(th);
+        let feats = document.createElement("th");
+        tbody.rows[5-i].appendChild(feats);
+    }
+    rows.forEach(r=>{
+        let name = r.cells[0].textContent;
+        let rating = ratingIndex(r.cells[1].textContent);
+        tbody.rows[5-rating].children[1].textContent += `${name}, `;
+    });
+    [...tbody.rows].forEach(r => r.children[1].textContent = r.children[1].textContent.replace(/, $/, ""));
+}
