4ebb6c633a4f708473093d246b91677328c28fe9
Troy Pracy <troyp7@gmail.com>
2023-10-22 16:07:25 +1100

    Fix :thesaurus_power

--- apps.js
+++ apps.js
@@ -181,6 +181,7 @@ apps.pw = {
 apps.ref = {
     thes: {
         power: async function(word) {
+            if (Array.isArray (word)) word = word.join(" ").trim();
             const ua = `"Mozilla/5.0 (X11; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0"`;
             const cmd = `curl -sA ${ua} "https://www.powerthesaurus.org/${word}/synonyms"`
                   + `| pup "#content-list #primary-area a text{}" | less`;
