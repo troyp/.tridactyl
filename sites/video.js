68269debad46601ca7d6cc74105b3e62b5d01694
Troy Pracy <troyp7@gmail.com>
2024-09-22 18:23:58 +1000

    Add free-to-air video apps list

--- sites/video.js
+++ sites/video.js
@@ -5,3 +5,11 @@ sites.LISTS.video_apps = [
     "https://www.disneyplus.com/en-gb/home",
     "https://www.youtube.com/",
 ];
+
+sites.LISTS.video_apps_freetoair = [
+    "https://iview.abc.net.au/",
+    "https://www.sbs.com.au/ondemand/",
+    "https://7plus.com.au/",
+    "https://www.9now.com.au/",
+    "https://10play.com.au/",
+];
